f48473b3d55245d9c119fd8f8bf5b10c1e6d288b
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-05-13 17:14:43 +0900

    feat : 비밀번호 암호화 코드 추가

--- src/main/java/yu/likelion14th/be_9th_lab/domains/auth/service/AuthService.java
+++ src/main/java/yu/likelion14th/be_9th_lab/domains/auth/service/AuthService.java
@@ -9,12 +9,14 @@ import yu.likelion14th.be_9th_lab.domains.auth.dto.response.SignInResDto;
 import yu.likelion14th.be_9th_lab.domains.user.entity.User;
 import yu.likelion14th.be_9th_lab.domains.user.service.UserService;
 import yu.likelion14th.be_9th_lab.security.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final UserService userService;
     private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public SignInResDto signIn(SignInReqDto signInRequest) {
@@ -23,7 +25,7 @@ public class AuthService {
 
         User user = userService.readByEmail(email);
 
-        if (!password.equals(user.getPassword())) {
+        if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new RuntimeException("비밀번호가 불일치합니다.");
         }
 
@@ -42,7 +44,12 @@ public class AuthService {
             throw new RuntimeException("이미 존재하는 email 입니다.");
         }
 
-        User user = request.toEntity();
+        String encoded = passwordEncoder.encode(request.getPassword());
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .password(encoded)
+                .build();
 
         userService.save(user);
     }
--- src/main/java/yu/likelion14th/be_9th_lab/security/SecurityConfig.java
+++ src/main/java/yu/likelion14th/be_9th_lab/security/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -32,5 +34,10 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);      // JwtAuthenticationFilter를 적용
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
